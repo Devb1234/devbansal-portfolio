0e55d6f2b7fc537d7ee6559205d9b64750b90b5b
Dev <221b137@juetguna.in>
2025-05-26 02:45:01 +0530

    Updates

---
+++ precache-manifest.3e77c0653e9d2b2a9be75b2679a451fc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c8d19d6837cb07a183c52acc757b0659",
+    "url": "/devbansal-portfolio/index.html"
+  },
+  {
+    "revision": "57dfa41b106f4e6cb72c",
+    "url": "/devbansal-portfolio/static/css/main.db683005.chunk.css"
+  },
+  {
+    "revision": "a0c0459ec5a67b3660fa",
+    "url": "/devbansal-portfolio/static/js/2.9683e878.chunk.js"
+  },
+  {
+    "revision": "57dfa41b106f4e6cb72c",
+    "url": "/devbansal-portfolio/static/js/main.2ad027a3.chunk.js"
+  },
+  {
+    "revision": "c9b71265de00c117a83e",
+    "url": "/devbansal-portfolio/static/js/runtime~main.1a722630.js"
+  }
+]);
